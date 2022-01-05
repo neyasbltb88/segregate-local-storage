928345ccb37a8d790a0061031a3f4e345c436c13
Neyasbltb <neyasbltb_88@mail.ru>
2022-01-06 01:53:02 +0300

    На демо-страницу добавлено несколько примеров использования

--- src/demo.js
+++ src/demo.js
@@ -2,5 +2,47 @@ import './demo.css';
 import SegregateLocalStorage from './index';
 window.SegregateLocalStorage = SegregateLocalStorage;
 
-let s = new SegregateLocalStorage();
-s.test();
+const codeStorage = document.querySelector('.code.storage');
+const btnLastUpdate = document.querySelector('.btn.lastUpdate');
+const btnNewProp = document.querySelector('.btn.newProp');
+const btnRemoveNewProp = document.querySelector('.btn.removeNewProp');
+const btnClearStorage = document.querySelector('.btn.clearStorage');
+
+const dateToString = (date) => {
+    let result = `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
+
+    return result;
+};
+
+const initialState = {
+    moduleName: 'TestStorageModule',
+    lastUpdate: '06.01.2022 00:44:45'
+};
+window.initialState = initialState;
+
+let sls = new SegregateLocalStorage('TestStorageModule', initialState);
+window.sls = sls;
+
+const updateCodeStorage = () => {
+    let storageData = sls.getAll();
+    let content = JSON.stringify(storageData, ' ', 4);
+    codeStorage.textContent = content;
+};
+updateCodeStorage();
+
+btnLastUpdate.addEventListener('click', () => {
+    sls.set('lastUpdate', dateToString(new Date()));
+    updateCodeStorage();
+});
+btnNewProp.addEventListener('click', () => {
+    sls.set('newProp', true);
+    updateCodeStorage();
+});
+btnRemoveNewProp.addEventListener('click', () => {
+    sls.remove('newProp');
+    updateCodeStorage();
+});
+btnClearStorage.addEventListener('click', () => {
+    sls.clear();
+    updateCodeStorage();
+});
