10180ef9ba71b8fe10efdca3f57345a2577e0681
Neyasbltb <neyasbltb_88@mail.ru>
2023-03-28 00:46:32 +0300

    Добавлена утилита CustomStorage

---
+++ src/utils/CustomStorage.ts
@@ -0,0 +1,50 @@
+class CustomStorage {
+    protected storage: Map<string, string> = new Map();
+    // readonly length: number;
+
+    get length(): number {
+        return this.storage.size;
+    }
+
+    clear() {
+        console.log('CustomStorage clear');
+
+        return this.storage.clear();
+    }
+
+    getItem(key: string) {
+        console.log('CustomStorage getItem: ', key);
+
+        return this.storage.get(key);
+    }
+
+    key(index: number): string | null {
+        console.log('CustomStorage key:', index);
+
+        return this.getItem([...this.storage.keys()][index]);
+    }
+
+    removeItem(key: string) {
+        console.log('CustomStorage removeItem:', key);
+
+        return this.storage.delete(key);
+    }
+
+    setItem(key: string, value: string) {
+        console.log('CustomStorage setItem:', { key, value });
+        return this.storage.set(key, value);
+    }
+
+    toString() {
+        return 'CustomStorage';
+    }
+
+    toJSON() {
+        const res: Record<string, string> = {};
+        this.storage.forEach((value, key) => (res[key] = value));
+
+        return res;
+    }
+}
+
+export default CustomStorage;
